a6f7c86b13f06b49df3780c0f4d35ee875b1243e
Poornima Rajendhran <poornimarajendhran@Poornimas-Air.home>
2020-06-19 08:03:17 -0400

    Spring cloud gateway skeleton setup

---
+++ src/main/java/com/my/poc/springcloudgatewaycircuitbreaker/SpringCloudGatewayCircuitBreakerApplication.java
@@ -0,0 +1,13 @@
+package com.my.poc.springcloudgatewaycircuitbreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayCircuitBreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayCircuitBreakerApplication.class, args);
+	}
+
+}
